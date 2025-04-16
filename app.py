2fc2f3de7b0a18faa4b6e90c4fa3fad54a7ac1de
chriswalkerkun <chriswalkerkun@gmail.com>
2025-04-16 16:48:40 +0700

    finally

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import app, db
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create tables if they don't exist
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()  # Create tables if they don't exist
+    db.session.commit() # Commit the changes to database
+app.run(debug=True)
