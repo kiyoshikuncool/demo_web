d5060c9945822f7c5c43bf42dd9e536ee4c774c5
chriswalkerkun <chriswalkerkun@gmail.com>
2025-04-16 12:10:45 +0700

    cmt

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from app.config import Config
 
-app = Flask(__name__, template_folder='../templates', static_folder="../static")
+app = Flask(__name__, template_folder='../templates', static_folder="../static") # Cấu hình cho FEFE
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
+# Route của FE
 @app.get('/index')
 def index():
     return render_template('index.html')
@@ -23,6 +24,6 @@ def signup():
 def dashboard():
     return render_template('dashboard.html')
 
-# Import and register routes
+# Import and register routes (API)
 from app.controllers.user import user_bp
 app.register_blueprint(user_bp)
