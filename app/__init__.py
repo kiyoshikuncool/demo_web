c9bd20580ebf67fe175c6d1fac34dc2feb5d0dba
chriswalkerkun <chriswalkerkun@gmail.com>
2025-04-17 07:47:46 +0700

    ew

--- app/__init__.py
+++ app/__init__.py
@@ -8,31 +8,28 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-def create_app():
-    app = Flask(__name__, template_folder='../templates', static_folder="../static") # Cấu hình cho FE
-    app.config.from_object(Config)
+app = Flask(__name__, template_folder='../templates', static_folder="../static") # Cấu hình cho FE
+app.config.from_object(Config)
 
-    # Initialize the extensions with the app
-    db.init_app(app)
-    bcrypt.init_app(app)
-    jwt.init_app(app)
+# Initialize the extensions with the app
+db.init_app(app)
+bcrypt.init_app(app)
+jwt.init_app(app)
 
-    # Route của FE
-    @app.get('/')
-    @app.get('/index')
-    def index():
-        return render_template('index.html')
+# Route của FE
+@app.get('/')
+@app.get('/index')
+def index():
+    return render_template('index.html')
 
-    @app.get('/signup')
-    def signup():
-        return render_template('register.html')
+@app.get('/signup')
+def signup():
+    return render_template('register.html')
 
-    @app.get('/dashboard')
-    def dashboard():
-        return render_template('dashboard.html')
+@app.get('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
-    # Import and register routes (API)
-    from app.controllers.user import user_bp
-    app.register_blueprint(user_bp)
-    
-    return app
+# Import and register routes (API)
+from app.controllers.user import user_bp
+app.register_blueprint(user_bp)
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
-from app import app, create_app, db
-
-app = create_app()
+from app import app, db
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
-    app.run(debug=True)
+    app.run(debug=False)
