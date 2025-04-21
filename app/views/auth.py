08cee93a4dacc290a51c9a6b80729fccea7dbb1e
chriswalkerkun <chriswalkerkun@gmail.com>
2025-04-21 10:23:04 +0700

    editedit

--- app/views/auth.py
+++ app/views/auth.py
@@ -5,12 +5,12 @@ auth_view_bp = Blueprint('auth', __name__)
 
 @auth_view_bp.get('/login')
 def index():
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
-@auth_view_bp.get('/signup')
+@auth_view_bp.get('/register')
 def signup():
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 @auth_view_bp.get('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('auth/dashboard.html')
--- app/views/landing.py
+++ app/views/landing.py
@@ -6,3 +6,7 @@ landing_view_bp = Blueprint('landing', __name__)
 @landing_view_bp.get('/')
 def landing():
     return render_template('landing.html')
+
+@landing_view_bp.get('/select-role')
+def selectRole():
+    return render_template('select-role.html')
